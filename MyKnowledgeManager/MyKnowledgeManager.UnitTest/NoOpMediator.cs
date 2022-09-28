d1917e1b385c5fc62cc1fd650445545495ab122c
AmirHossein ShabaniNejad <amirshabani17@outlook.com>
2022-09-28 19:43:27 +0330

    UnitTest for KnowedgeConstructor succeeded.

---
+++ MyKnowledgeManager/MyKnowledgeManager.UnitTest/NoOpMediator.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyKnowledgeManager.UnitTest
+{
+    public class NoOpMediator : IMediator
+    {
+        public async IAsyncEnumerable<TResponse> CreateStream<TResponse>(IStreamRequest<TResponse> request, 
+            CancellationToken cancellationToken = default)
+        {
+            await Task.CompletedTask;
+            yield break;
+        }
+
+        public async IAsyncEnumerable<object?> CreateStream(object request, CancellationToken cancellationToken = default)
+        {
+            await Task.CompletedTask;
+            yield break;
+        }
+
+        public Task Publish(object notification, CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default) where TNotification : INotification
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult<TResponse>(default);
+        }
+
+        public Task<object?> Send(object request, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult<object?>(default);
+        }
+    }
+}
